d1e6a594abd46d9e804b0ca44f4ddb4cf039bde1
Daryna Riabova <riabovadaryna@gmail.com>
2025-12-28 18:12:52 +0100

    12.1

---
+++ lesson 12/12.1.py
@@ -0,0 +1,15 @@
+import codecs
+import re
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+    cleaned_text = re.sub(r'<.*?>', '', html)
+    lines = cleaned_text.splitlines()
+    non_empty_lines = [line.strip() for line in lines if line.strip()]
+    final_text = '\n'.join(non_empty_lines)
+    with codecs.open(result_file, 'w', 'utf-8') as file:
+        file.write(final_text)
+
+delete_html_tags('draft.html')
+
+
